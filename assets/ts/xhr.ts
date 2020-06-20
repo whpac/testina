b5808cf45789a70a0737823357060e848bac9051
whpac <marcinszwarc@hotmail.com>
2020-06-20 13:18:12 +0200

    Obsługa żądań z zawartością oraz parsowanie JSON

--- assets/ts/xhr.ts
+++ assets/ts/xhr.ts
@@ -1,13 +1,20 @@
 type XHRResult = {
     Status: number,
     StatusText: string,
-    ResponseText: string
+    Response: {}
 };
 
-export function Request(url: string, method?: string){
+/**
+ * Performs a request to the server and returns a response parsed as a JSON
+ * @param url URL to make a request to
+ * @param method Request method (default is GET)
+ * @param request_data Object containing the request data, will be serialized as a JSON
+ */
+export function Request(url: string, method?: string, request_data?: any){
     return new Promise<XHRResult>((resolve, reject) => {
         let xhr = new XMLHttpRequest();
         xhr.open(method ?? 'GET', url, true);
+        xhr.setRequestHeader('Accept', 'application/json');
         xhr.onreadystatechange = () => {
             if(xhr.readyState != 4) return;
 
@@ -15,16 +22,24 @@ export function Request(url: string, method?: string){
                 resolve({
                     Status: xhr.status,
                     StatusText: xhr.statusText,
-                    ResponseText: xhr.responseText
+                    Response: JSON.parse(xhr.responseText)
                 });
             }else{
                 reject({
                     Status: xhr.status,
                     StatusText: xhr.statusText,
-                    ResponseText: ''
+                    Response: {}
                 });
             }
         };
-        xhr.send();
+
+        // Serialize the request data
+        let serialized_data: (string | null) = null;
+        if(request_data !== undefined){
+            serialized_data = JSON.stringify(request_data);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+        }
+
+        xhr.send(serialized_data);
     });
 }
