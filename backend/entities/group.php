06ceac0ddccb48afc791183a5000ed599e0f8c21
Marcin Szwarc <marcinszwarc@hotmail.com>
2020-07-17 19:31:51 +0200

    Zintegrowano klasę Group z bazą danych

--- backend/entities/group.php
+++ backend/entities/group.php
@@ -4,11 +4,47 @@ namespace Entities;
 use \UEngine\Modules\Core\RichException;
 use \UEngine\Modules\Core\Database\DatabaseManager;
 
-class Group{
-    protected $id;
+define('TABLE_GROUPS', 'groups');
 
-    public function GetId(){
+class Group extends Entity{
+    protected /* int */ $id;
+    protected /* string */ $name;
+
+    protected static /* string */ function GetTableName(){
+        return TABLE_GROUPS;
+    }
+
+    protected static /* bool */ function AllowsDeferredFetch(){
+        return true;
+    }
+
+    protected /* void */ function OnPopulate(){
+        settype($this->id, 'int');
+        settype($this->name, 'string');
+    }
+
+    public /* int */ function GetId(){
         return $this->id;
+    }
+
+    public /* string */ function GetName(){
+        $this->FetchIfNeeded();
+        return $this->name;
+    }
+
+    public static /* Group[] */ function GetAll(){
+        $groups = [];
+        $result = DatabaseManager::GetProvider()
+                ->Table(TABLE_GROUPS)
+                ->Select()
+                ->Run();
+
+        for($i=0; $i<$result->num_rows; $i++){
+            $row = $result->fetch_assoc();
+            $groups[] = new Group($row);
+        }
+
+        return $groups;
     }
 }
 ?>
