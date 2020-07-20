3d908218e7d1bb8771cd38decc086459fc56f302
Marcin Szwarc <marcinszwarc@hotmail.com>
2020-07-20 15:37:31 +0200

    Rodzielono przypisane dla mnie od przeze mnie

--- backend/api/resources/assignmentcollection.php
+++ backend/api/resources/assignmentcollection.php
@@ -5,13 +5,32 @@ class AssignmentCollection extends Resource {
 
     protected function LazyLoad($data, $test_id){
         $current_user = $this->GetContext()->GetUser();
+
         $assignments = \Entities\Assignment::GetAssignmentsForUser($current_user);
 
         foreach($assignments as $assignment){
             $this->AddSubResource($assignment->GetId(), new Assignment($assignment));
         }
 
+        $assignments = \Entities\Assignment::GetAssignedByUser($current_user);
+
+        foreach($assignments as $assignment){
+            $this->AddSubResource($assignment->GetId(), new Assignment($assignment));
+        }
+
         return true;
+    }
+
+    public function CreateSubResource(/* mixed */ $source){
+        $current_user = $this->GetContext()->GetUser();
+        $test = new \Entities\Test($source->test_id);
+        $attempt_limit = $source->attempt_limit;
+        $deadline = new \DateTime($source->time_limit);
+
+        $assignment = \Entities\Assignment::Create($current_user, $test, $attempt_limit, $deadline);
+
+        header('Content-Location: '.$assignment->GetId());
+        return null;
     }
 }
 ?>
--- backend/api/resources/root.php
+++ backend/api/resources/root.php
@@ -4,6 +4,7 @@ namespace Api\Resources;
 class Root extends Resource {
 
     protected function LazyLoad($data, $name){
+        $this->AddSubResource('assigned_to_me', new AssignedToMeCollection());
         $this->AddSubResource('assignments', new AssignmentCollection());
         $this->AddSubResource('groups', new GroupCollection());
         $this->AddSubResource('tests', new TestCollection());
