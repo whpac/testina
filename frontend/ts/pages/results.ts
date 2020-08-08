288b88073a10e261b544793a930c03329a925c54
Marcin Szwarc <marcinszwarc@hotmail.com>
2020-08-08 13:48:16 +0200

    Dodaję nagłówek do strony

--- frontend/ts/pages/results.ts
+++ frontend/ts/pages/results.ts
@@ -4,12 +4,27 @@ import AssignmentLoader from '../entities/loaders/assignmentloader';
 
 export default class ResultsPage extends Page{
     protected Assignment: Assignment | undefined;
+    protected TestNameHeading: Text;
+
+    constructor(){
+        super();
+
+        let page_heading = document.createElement('h1');
+        this.AppendChild(page_heading);
+        page_heading.appendChild(this.TestNameHeading = document.createTextNode(''));
+
+        let span_secondary = document.createElement('span');
+        span_secondary.classList.add('secondary');
+        span_secondary.textContent = ' – wyniki';
+        page_heading.appendChild(span_secondary);
+    }
 
     async LoadInto(container: HTMLElement, params?: any) {
         if(params === undefined) throw 'Nie podano testu, dla którego mają być wyświetlone wyniki.';
         if(typeof params === 'number') this.Assignment = await AssignmentLoader.LoadById(params);
         else this.Assignment = params as Assignment;
-        
+
+        this.TestNameHeading.textContent = this.Assignment.Test.Name;
         container.appendChild(this.Element);
     }
 
