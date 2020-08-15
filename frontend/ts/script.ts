6508f327259b5c7583796c29dc3bd1a3d551b179
Marcin Szwarc <marcinszwarc@hotmail.com>
2020-08-15 16:54:38 +0200

    Dodanie obsługi błędów

--- frontend/ts/script.ts
+++ frontend/ts/script.ts
@@ -11,25 +11,32 @@ import AboutPage from './pages/about';
 import AssignmentsPage from './pages/assignments';
 import ResultsPage from './pages/results';
 
-// Initialize the page manager
-let root = document.getElementById('content-container');
-if(root === null) throw 'Błąd ładowania';
-let loading_wrapper = new LoadingIndicator('loading-wrapper');
+try {
+    // Odwołanie do obiektu, gdzie będzie wyświetlana strona
+    let root = document.getElementById('content-container');
+    if(root === null) throw 'Błąd ładowania';
+    let loading_wrapper = new LoadingIndicator('loading-wrapper');
 
-Navbar.Draw();
-Navbar.AttachEventHandlers();
+    // Inicjalizacja paska nawigacji
+    Navbar.Draw();
+    Navbar.AttachEventHandlers();
 
-PageManager.Initialize(root, loading_wrapper);
-PageManager.AddPage('home', new HomePage(), false);
-PageManager.AddPage('informacje', new AboutPage(), false);
-PageManager.AddPage('testy/biblioteka', new LibraryPage(), false);
-PageManager.AddPage('testy/edytuj', new EditTestPage(), true);
-PageManager.AddPage('testy/lista', new AssignedTestsListPage(), false);
-PageManager.AddPage('testy/rozwiąż', new SolveTestPage(), true);
-PageManager.AddPage('testy/przypisane', new AssignmentsPage(), true);
-PageManager.AddPage('testy/wyniki', new ResultsPage(), true);
+    // Inicjalizacja menedżera stron
+    PageManager.Initialize(root, loading_wrapper);
+    PageManager.AddPage('home', new HomePage(), false);
+    PageManager.AddPage('informacje', new AboutPage(), false);
+    PageManager.AddPage('testy/biblioteka', new LibraryPage(), false);
+    PageManager.AddPage('testy/edytuj', new EditTestPage(), true);
+    PageManager.AddPage('testy/lista', new AssignedTestsListPage(), false);
+    PageManager.AddPage('testy/rozwiąż', new SolveTestPage(), true);
+    PageManager.AddPage('testy/przypisane', new AssignmentsPage(), true);
+    PageManager.AddPage('testy/wyniki', new ResultsPage(), true);
 
-let initial_page = 'home';
-let body_url = document.body.dataset.url;
-if(body_url !== undefined && body_url != '') initial_page = body_url;
-PageManager.GoToPage(initial_page, undefined, true);
+    // Załaduj stronę początkową
+    let initial_page = 'home';
+    let body_url = document.body.dataset.url;
+    if(body_url !== undefined && body_url != '') initial_page = body_url;
+    PageManager.GoToPage(initial_page, undefined, true);
+} catch(e) {
+    alert('Wystąpił błąd: ' + e.toString() + '.');
+}
