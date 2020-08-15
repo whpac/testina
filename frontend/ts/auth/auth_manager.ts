5a1a2f77f8376e23d2b729805b5f4a417ce74e44
Marcin Szwarc <marcinszwarc@hotmail.com>
2020-08-15 17:47:19 +0200

    Poprawka na asynchroniczność

--- frontend/ts/auth/auth_manager.ts
+++ frontend/ts/auth/auth_manager.ts
@@ -2,8 +2,8 @@ import UserLoader from '../entities/loaders/userloader';
 
 export default class AuthManager {
 
-    static IsAuthorized() {
-        return this.GetCurrentUser(true) !== undefined;
+    static async IsAuthorized() {
+        return (await this.GetCurrentUser(true)) !== undefined;
     }
 
     static async GetCurrentUser(force: boolean = false) {
