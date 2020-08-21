d4d9996973531775b09e77f81dda2aa0733190d9
Marcin Szwarc <marcinszwarc@hotmail.com>
2020-08-21 20:24:13 +0200

    Dołączono bibliotekę JS-Levenshtein

---
+++ frontend/ts/utils/levenshtein.ts
@@ -0,0 +1,141 @@
+/**
+ * Kod pochodzi z biblioteki
+ * JS-Levenshtein, wersja 1.1.6
+ * https://github.com/gustf/js-levenshtein
+ * autorstwa Gustafa Anderssona
+ * na licencji MIT
+ * Adnotacje typów i opis funkcji dodał Marcin Szwarc
+ * 
+ * Source code from
+ * JS-Levenshtein, v1.1.6
+ * https://github.com/gustf/js-levenshtein
+ * by Gustaf Andersson
+ * MIT license
+ * Type annotations and function description added by Marcin Szwarc
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2017 Gustaf Andersson
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * Oblicza odległość Levenshteina między dwoma ciągami znaków
+ * @param a Pierwszy ciąg znaków
+ * @param b Drugi ciąg znaków
+ */
+export default function Levenshtein(a: string, b: string): number {
+    if(a === b) {
+        return 0;
+    }
+
+    if(a.length > b.length) {
+        let tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    let la: number = a.length;
+    let lb: number = b.length;
+
+    while(la > 0 && (a.charCodeAt(la - 1) === b.charCodeAt(lb - 1))) {
+        la--;
+        lb--;
+    }
+
+    let offset: number = 0;
+
+    while(offset < la && (a.charCodeAt(offset) === b.charCodeAt(offset))) {
+        offset++;
+    }
+
+    la -= offset;
+    lb -= offset;
+
+    if(la === 0 || lb < 3) {
+        return lb;
+    }
+
+    let x: number = 0;
+    let y: number;
+    let d0: number;
+    let d1: number;
+    let d2: number;
+    let d3: number;
+    let dd!: number;
+    let dy: number;
+    let ay: number;
+    let bx0: number;
+    let bx1: number;
+    let bx2: number;
+    let bx3: number;
+
+    let vector: number[] = [];
+
+    for(y = 0; y < la; y++) {
+        vector.push(y + 1);
+        vector.push(a.charCodeAt(offset + y));
+    }
+
+    let len: number = vector.length - 1;
+
+    for(; x < lb - 3;) {
+        bx0 = b.charCodeAt(offset + (d0 = x));
+        bx1 = b.charCodeAt(offset + (d1 = x + 1));
+        bx2 = b.charCodeAt(offset + (d2 = x + 2));
+        bx3 = b.charCodeAt(offset + (d3 = x + 3));
+        dd = (x += 4);
+        for(y = 0; y < len; y += 2) {
+            dy = vector[y];
+            ay = vector[y + 1];
+            d0 = _min(dy, d0, d1, bx0, ay);
+            d1 = _min(d0, d1, d2, bx1, ay);
+            d2 = _min(d1, d2, d3, bx2, ay);
+            dd = _min(d2, d3, dd, bx3, ay);
+            vector[y] = dd;
+            d3 = d2;
+            d2 = d1;
+            d1 = d0;
+            d0 = dy;
+        }
+    }
+
+    for(; x < lb;) {
+        bx0 = b.charCodeAt(offset + (d0 = x));
+        dd = ++x;
+        for(y = 0; y < len; y += 2) {
+            dy = vector[y];
+            vector[y] = dd = _min(dy, d0, dd, bx0, vector[y + 1]);
+            d0 = dy;
+        }
+    }
+
+    return dd;
+}
+
+function _min(d0: number, d1: number, d2: number, bx: number, ay: number) {
+    return d0 < d1 || d2 < d1
+        ? d0 > d2
+            ? d2 + 1
+            : d0 + 1
+        : bx === ay
+            ? d1
+            : d1 + 1;
+}
