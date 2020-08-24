9ad8bdf279f0000210dab40a343f99f7d2833750
Marcin Szwarc <marcinszwarc@hotmail.com>
2020-08-24 18:16:17 +0200

    Dodano okienko z licencją do bibliotek

---
+++ frontend/ts/components/about/credits_dialog.ts
@@ -0,0 +1,76 @@
+import Dialog from '../basic/dialog';
+import Icon from '../basic/icon';
+import LicenseDialog from './license_dialog';
+import { License } from '../../utils/license_store';
+
+export default class CreditsDialog extends Dialog {
+    protected RowsContainer: HTMLElement;
+
+    constructor() {
+        super();
+
+        this.SetHeader('Biblioteki open-source');
+
+        this.RowsContainer = document.createElement('div');
+        this.AddContent(this.RowsContainer);
+
+        let close_button = document.createElement('button');
+        close_button.textContent = 'Zamknij';
+        close_button.addEventListener('click', this.Hide.bind(this));
+        this.AddButton(close_button);
+
+        const libs = [
+            {
+                name: 'FontAwesome',
+                author: 'Dave Gandy',
+                license: License.SIL_OpenFontLicense
+            },
+            {
+                name: 'JS-Levenshtein',
+                author: 'Gustaf Andersson',
+                license: License.MIT
+            },
+            {
+                name: 'Roboto',
+                author: 'Christian Robertson',
+                license: License.ApacheLicense2
+            },
+            {
+                name: 'Utf8ToExtendedAscii',
+                author: 'PHP Documentation Group',
+                license: License.CreativeCommons_BY_3
+            }
+        ];
+        for(let lib of libs) {
+            this.AddLibrary(lib.name, lib.author, lib.license);
+        }
+    }
+
+    protected AddLibrary(name: string, author: string, license: License) {
+        let row_wrapper = document.createElement('div');
+        this.RowsContainer.appendChild(row_wrapper);
+        row_wrapper.classList.add('row-wrapper');
+
+        let lib_name = document.createElement('span');
+        row_wrapper.appendChild(lib_name);
+        lib_name.classList.add('row-heading');
+        lib_name.textContent = name;
+
+        let lib_author = document.createElement('span');
+        row_wrapper.appendChild(lib_author);
+        lib_author.classList.add('row-description');
+        lib_author.appendChild((new Icon('user-o')).GetElement());
+        lib_author.appendChild(document.createTextNode(' ' + author));
+
+        let lib_license = document.createElement('button');
+        row_wrapper.appendChild(lib_license);
+        lib_license.classList.add('row-button');
+        lib_license.appendChild((new Icon('balance-scale')).GetElement());
+        lib_license.title = 'Wyświetl licencję';
+        lib_license.addEventListener('click', (() => {
+            let license_dialog = new LicenseDialog();
+            license_dialog.Populate(license);
+            license_dialog.Show();
+        }).bind(this));
+    }
+}
---
+++ frontend/ts/components/about/license_dialog.ts
@@ -0,0 +1,27 @@
+import Dialog from '../basic/dialog';
+import LicenseStore, { License } from '../../utils/license_store';
+
+export default class LicenseDialog extends Dialog {
+    protected LicenseText: HTMLElement;
+
+    constructor() {
+        super();
+        this.DialogElement.classList.add('rich');
+
+        this.SetHeader('Treść licencji');
+
+        this.LicenseText = document.createElement('section');
+        this.LicenseText.style.fontFamily = 'serif';
+        this.LicenseText.style.whiteSpace = 'pre-wrap';
+        this.AddContent(this.LicenseText);
+
+        let close_button = document.createElement('button');
+        close_button.textContent = 'Zamknij';
+        close_button.addEventListener('click', this.Hide.bind(this));
+        this.AddButton(close_button);
+    }
+
+    public Populate(license: License) {
+        this.LicenseText.textContent = LicenseStore.GetLicenseText(license);
+    }
+}
