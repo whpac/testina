873246567e5073143f01889874231f2a9ac9e9cd
Marcin Szwarc <marcinszwarc@hotmail.com>
2020-09-10 11:32:21 +0200

    Obsługa zmiany kolejności pytań

--- frontend/ts/components/surveys/survey_question_card.ts
+++ frontend/ts/components/surveys/survey_question_card.ts
@@ -2,27 +2,60 @@ import Card from '../basic/card';
 import Question from '../../entities/question';
 import { AutoGrow } from '../../utils/elementutils';
 import AnswerWrapper from './answer_wrapper';
+import Icon from '../basic/icon';
 
-export default class SurveyQuestionCard extends Card {
+export default class SurveyQuestionCard extends Card<"moveup" | "movedown"> {
+    protected EditMode: boolean;
     protected Question: Question | undefined;
+    protected _IsFirst: boolean = false;
+    protected _IsLast: boolean = false;
+
     protected QuestionNumberText: Text;
     protected QuestionTypeSelect: HTMLSelectElement | undefined;
+    protected MoveUpButton: HTMLButtonElement | undefined;
+    protected MoveDownButton: HTMLButtonElement | undefined;
     protected HeadingField: HTMLTextAreaElement | HTMLHeadingElement;
     protected FooterField: HTMLTextAreaElement | HTMLParagraphElement;
     protected AnswerWrapper: AnswerWrapper;
 
+    public PreviousCard: SurveyQuestionCard | null;
+    public NextCard: SurveyQuestionCard | null;
+
+    public get IsFirst() {
+        return this._IsFirst;
+    }
+    public set IsFirst(value: boolean) {
+        this._IsFirst = value;
+        if(this.MoveUpButton !== undefined)
+            this.MoveUpButton.disabled = value;
+    }
+
+    public get IsLast() {
+        return this._IsLast;
+    }
+    public set IsLast(value: boolean) {
+        this._IsLast = value;
+        if(this.MoveDownButton !== undefined)
+            this.MoveDownButton.disabled = value;
+    }
+
     public constructor(edit_mode: boolean = false) {
         super();
+        this.EditMode = edit_mode;
 
-        let question_number = document.createElement('div');
-        this.AppendChild(question_number);
-        question_number.style.marginBottom = '3px';
-        question_number.textContent = 'Pytanie ';
-        question_number.appendChild(this.QuestionNumberText = document.createTextNode('0'));
+        this.PreviousCard = null;
+        this.NextCard = null;
+
+        let question_header = document.createElement('div');
+        question_header.classList.add('survey-question-header');
+        this.AppendChild(question_header);
+        question_header.style.marginBottom = '3px';
+        question_header.textContent = 'Pytanie ';
+        question_header.appendChild(this.QuestionNumberText = document.createTextNode('0'));
         if(edit_mode) {
-            question_number.appendChild(document.createTextNode(': '));
+            question_header.appendChild(document.createTextNode(': '));
             this.QuestionTypeSelect = document.createElement('select');
-            question_number.appendChild(this.QuestionTypeSelect);
+            question_header.appendChild(this.QuestionTypeSelect);
             this.QuestionTypeSelect.addEventListener('change', this.ChangeQuestionType.bind(this));
 
             let types: [number, string][] = [
@@ -37,12 +70,36 @@ export default class SurveyQuestionCard extends Card {
                 option.textContent = type[1];
                 this.QuestionTypeSelect.appendChild(option);
             }
+
+            let buttons = document.createElement('span');
+            buttons.classList.add('buttons');
+            question_header.appendChild(buttons);
+
+            this.MoveUpButton = document.createElement('button');
+            buttons.appendChild(this.MoveUpButton);
+            this.MoveUpButton.classList.add('secondary', 'control-button');
+            this.MoveUpButton.appendChild(new Icon('arrow-up').GetElement());
+            this.MoveUpButton.addEventListener('click', this.MoveUp.bind(this));
+            this.MoveUpButton.title = 'Przenieś pytanie wcześniej';
+
+            this.MoveDownButton = document.createElement('button');
+            buttons.appendChild(this.MoveDownButton);
+            this.MoveDownButton.classList.add('secondary', 'control-button');
+            this.MoveDownButton.appendChild(new Icon('arrow-down').GetElement());
+            this.MoveDownButton.addEventListener('click', this.MoveDown.bind(this));
+            this.MoveDownButton.title = 'Przenieś pytanie dalej';
+
+            let remove_btn = document.createElement('button');
+            buttons.appendChild(remove_btn);
+            remove_btn.classList.add('error');
+            remove_btn.appendChild(new Icon('trash').GetElement());
+            remove_btn.title = 'Usuń pytanie';
         } else {
-            question_number.classList.add('secondary');
-            question_number.appendChild(document.createTextNode('.'));
+            question_header.classList.add('secondary');
+            question_header.appendChild(document.createTextNode('.'));
         }
 
-        this.AnswerWrapper = new AnswerWrapper(true);
+        this.AnswerWrapper = new AnswerWrapper(this.EditMode);
 
         if(edit_mode) {
             let heading = document.createElement('textarea');
@@ -66,7 +123,7 @@ export default class SurveyQuestionCard extends Card {
     }
 
     public Populate(question: Question, question_number: number) {
-        this.QuestionNumberText.textContent = question_number.toString();
+        this.SetNumber(question_number);
 
         if(this.HeadingField instanceof HTMLInputElement) {
             this.HeadingField.value = question.Text;
@@ -79,8 +136,28 @@ export default class SurveyQuestionCard extends Card {
         this.AnswerWrapper.Populate(question);
     }
 
+    public SetNumber(question_number: number) {
+        this.QuestionNumberText.textContent = question_number.toString();
+    }
+
     protected ChangeQuestionType() {
         if(this.QuestionTypeSelect === undefined) return;
         this.AnswerWrapper.ChangeType(parseInt(this.QuestionTypeSelect.value));
+    }
+
+    protected MoveUp() {
+        let prev_question = this.Element.previousElementSibling;
+        if(prev_question === null) return;
+        let parent = this.Element.parentElement;
+        parent?.insertBefore(this.Element, prev_question);
+        this.FireEvent('moveup');
+    }
+
+    protected MoveDown() {
+        let next_question = this.Element.nextElementSibling;
+        if(next_question === null) return;
+        let parent = this.Element.parentElement;
+        parent?.insertBefore(next_question, this.Element);
+        this.FireEvent('movedown');
     }
 }
--- frontend/ts/pages/edit_survey.ts
+++ frontend/ts/pages/edit_survey.ts
@@ -3,14 +3,20 @@ import Test from '../entities/test';
 import SurveyLoader from '../entities/loaders/surveyloader';
 import SurveyIntroduction from '../components/surveys/survey_introduction';
 import SurveyQuestionCard from '../components/surveys/survey_question_card';
+import Icon from '../components/basic/icon';
+import { MoveElement } from '../utils/arrayutils';
+import Question from '../entities/question';
 
 export default class EditSurveyPage extends Page {
     protected Survey: Test | undefined;
     protected SurveyNameHeading: Text;
     protected IntroductionCard: SurveyIntroduction;
+    protected QuestionCards: SurveyQuestionCard[];
 
     public constructor() {
         super();
+
+        this.QuestionCards = [];
 
         let heading = document.createElement('h1');
         this.AppendChild(heading);
@@ -35,11 +41,18 @@ export default class EditSurveyPage extends Page {
 
         let questions = await this.Survey.GetQuestions();
         for(let i = 0; i < questions.length; i++) {
-            let question = questions[i];
-            let question_card = new SurveyQuestionCard(true);
-            this.AppendChild(question_card);
-            question_card.Populate(question, i + 1);
+            this.RenderQuestion(questions[i], i + 1);
         }
+        this.RefreshQuestionOrder();
+
+        let new_question_btn_wrapper = document.createElement('div');
+        new_question_btn_wrapper.classList.add('center');
+        this.AppendChild(new_question_btn_wrapper);
+
+        let new_question_btn = document.createElement('button');
+        new_question_btn.appendChild(new Icon('plus').GetElement());
+        new_question_btn.appendChild(document.createTextNode(' Dodaj pytanie'));
+        new_question_btn_wrapper.appendChild(new_question_btn);
 
         container.appendChild(this.Element);
     }
@@ -54,5 +67,76 @@ export default class EditSurveyPage extends Page {
 
     async GetTitle() {
         return 'Edytuj: ' + this.Survey?.Name;
+    }
+
+    protected RenderQuestion(question: Question, question_number: number) {
+        let question_card = new SurveyQuestionCard(true);
+        this.AppendChild(question_card);
+        question_card.Populate(question, question_number);
+        this.QuestionCards.push(question_card);
+        question_card.AddEventListener('moveup', (() => this.OnQuestionMovedUp(question_card)).bind(this));
+        question_card.AddEventListener('movedown', (() => this.OnQuestionMovedDown(question_card)).bind(this));
+
+        if(this.QuestionCards.length >= 2) {
+            let prev = this.QuestionCards[this.QuestionCards.length - 2];
+            prev.NextCard = question_card;
+            question_card.PreviousCard = prev;
+        }
+    }
+
+    protected RefreshQuestionOrder() {
+        for(let i = 0; i < this.QuestionCards.length; i++) {
+            this.QuestionCards[i].IsFirst = (i == 0);
+            this.QuestionCards[i].IsLast = (i == this.QuestionCards.length - 1);
+            this.QuestionCards[i].SetNumber(i + 1);
+        }
+    }
+
+    protected OnQuestionMovedUp(question_card: SurveyQuestionCard) {
+        if(question_card.PreviousCard === null) return;
+
+        let former_prev = question_card.PreviousCard;
+        let new_prev = former_prev.PreviousCard;
+        let former_next = question_card.NextCard;
+
+        if(new_prev !== null) new_prev.NextCard = question_card;
+        former_prev.PreviousCard = question_card;
+        former_prev.NextCard = former_next;
+        question_card.PreviousCard = new_prev;
+        question_card.NextCard = former_prev;
+        if(former_next !== null) former_next.PreviousCard = former_prev;
+
+        for(let i = 0; i < this.QuestionCards.length; i++) {
+            if(question_card == this.QuestionCards[i]) {
+                MoveElement(this.QuestionCards, i, i - 1);
+                break;
+            }
+        }
+
+        this.RefreshQuestionOrder();
+    }
+
+    protected OnQuestionMovedDown(question_card: SurveyQuestionCard) {
+        if(question_card.NextCard === null) return;
+
+        let former_prev = question_card.PreviousCard;
+        let former_next = question_card.NextCard;
+        let new_next = former_next.NextCard;
+
+        if(former_prev !== null) former_prev.NextCard = former_next;
+        question_card.PreviousCard = former_next;
+        question_card.NextCard = new_next;
+        former_next.PreviousCard = former_prev;
+        former_next.NextCard = question_card;
+        if(new_next !== null) new_next.PreviousCard = question_card;
+
+        for(let i = 0; i < this.QuestionCards.length; i++) {
+            if(question_card == this.QuestionCards[i]) {
+                MoveElement(this.QuestionCards, i, i + 1);
+                break;
+            }
+        }
+
+        this.RefreshQuestionOrder();
     }
 }
