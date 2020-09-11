3637abc2db2fa4f37dc6ab0fc0e7afbbab37c459
Marcin Szwarc <marcinszwarc@hotmail.com>
2020-09-11 17:54:44 +0200

    Dodano klasę .icon

--- frontend/ts/components/basic/icon.ts
+++ frontend/ts/components/basic/icon.ts
@@ -3,23 +3,23 @@ import Component from './component';
 /**
  * Komponent z ikoną
  */
-export default class Icon extends Component{
+export default class Icon extends Component {
 
     /**
      * Komponent z ikoną
      * @param icon Nazwa ikony
      * @param classes Klasy CSS elementu HTML
      */
-    constructor(icon: string, ...classes: string[]){
+    constructor(icon: string, ...classes: string[]) {
         super();
 
         this.Element = document.createElement('span');
-        this.Element.classList.add('fa');
+        this.Element.classList.add('fa', 'icon');
         this.Element.classList.add(this.GetIconByKey(icon));
         if(classes !== undefined) this.Element.classList.add(...classes);
     }
 
-    protected GetIconByKey(icon_name: string){
+    protected GetIconByKey(icon_name: string) {
         return 'fa-' + icon_name;
     }
 }
