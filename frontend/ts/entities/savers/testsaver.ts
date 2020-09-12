69b1210d6e47dae081027ca0161a9c2a7338c163
Marcin Szwarc <marcinszwarc@hotmail.com>
2020-09-12 12:11:47 +0200

    Opakowywanie błędów sieci w jeden wyjątek

--- frontend/ts/entities/savers/testsaver.ts
+++ frontend/ts/entities/savers/testsaver.ts
@@ -1,3 +1,6 @@
+import EntitySavingException from '../../exceptions/entity_saving';
+import FetchingErrorException from '../../exceptions/fetching_error';
+import MalformedResponseException from '../../exceptions/malformed_response';
 import * as XHR from '../../utils/xhr';
 import ApiEndpoints from '../loaders/apiendpoints';
 import Test from '../test';
@@ -15,9 +18,21 @@ export default class TestSaver {
             time_limit: test.TimeLimit,
             description: test.Description
         };
-        let result = await XHR.PerformRequest(ApiEndpoints.GetEntityUrl(test), 'PUT', request_data);
-        if(result.Status != 204) {
-            throw result;
+        try {
+            let result = await XHR.PerformRequest(ApiEndpoints.GetEntityUrl(test), 'PUT', request_data);
+            if(result.Status != 204) {
+                let malformed_response = new MalformedResponseException('Serwer zwrócił nieoczekiwany kod odpowiedzi. Oczekiwano 204.', {
+                    ResponseText: '[Nieznany]',
+                    Status: result.Status,
+                    StatusText: result.StatusText
+                });
+                throw new EntitySavingException('Serwer zwrócił nieoczekiwany kod odpowiedzi. Oczekiwano 204.', malformed_response);
+            }
+        } catch(e) {
+            if(e instanceof FetchingErrorException || e instanceof MalformedResponseException) {
+                throw new EntitySavingException(e.Message, e);
+            }
+            throw e;
         }
     }
 }
