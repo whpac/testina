648ea1cd273682f44a754e8f88558c9bab20c9b1
Marcin Szwarc <marcinszwarc@hotmail.com>
2020-09-27 11:05:04 +0200

    Zmiana id użytkownika na string

--- backend/api/schemas/assignmentresults.php
+++ backend/api/schemas/assignmentresults.php
@@ -2,7 +2,7 @@
 namespace Api\Schemas;
 
 interface AssignmentResults{
-    public function user_id(): int;
+    public function user_id(): string;
     public function attempt_count(): int;
     public function last_attempt(): ?\DateTime;
     public function average_score(): ?int;
