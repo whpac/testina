a0604b7679c3e53f983dac363a44493603dcc09a
Marcin Szwarc <marcinszwarc@hotmail.com>
2020-10-05 16:10:49 +0200

    Dodano możliwość pobierania wyników ankiety

---
+++ frontend/ts/entities/loaders/surveyresultsloader.ts
@@ -0,0 +1,63 @@
+import * as XHR from '../../utils/xhr';
+import { Collection } from '../entity';
+import ApiEndpoints from './apiendpoints';
+import Test from '../test';
+import SurveyResults, { SurveyResultsClosedAnswer, SurveyResultsQuestion } from '../survey_results';
+
+/** Deskryptor podejścia w odpowiedzi z API */
+export interface SurveyResultsDescriptor {
+    questions: Collection<SurveyResultsQuestionDescriptor>;
+}
+
+interface SurveyResultsQuestionDescriptor {
+    id: number;
+    text: string;
+    answer_count: number;
+    user_supplied_answers: string[];
+    closed_answers: {
+        id: number;
+        text: string;
+        answer_count: number;
+    }[];
+}
+
+export default class SurveyResultsLoader {
+    /**
+     * Wczytuje wyniki ankiety
+     * @param survey Ankieta
+     */
+    public static async Load(survey: Test) {
+        let response = await XHR.PerformRequest(ApiEndpoints.GetEntityUrl(survey) + '/results?depth=5', 'GET');
+        let json = response.Response as SurveyResultsDescriptor;
+        return this.CreateFromDescriptor(json);
+    }
+
+    /**
+     * Tworzy wyniki na podstawie deskryptora
+     * @param results_descriptor Deskryptor wyników
+     */
+    public static CreateFromDescriptor(results_descriptor: SurveyResultsDescriptor) {
+        let questions: SurveyResultsQuestion[] = [];
+        for(let question_id in results_descriptor.questions) {
+            let question = results_descriptor.questions[question_id];
+
+            let answers: SurveyResultsClosedAnswer[] = [];
+            for(let closed_answer of question.closed_answers) {
+                answers.push(new SurveyResultsClosedAnswer(
+                    closed_answer.id,
+                    closed_answer.text,
+                    closed_answer.answer_count
+                ));
+            }
+
+            questions.push(new SurveyResultsQuestion(
+                question.id,
+                question.text,
+                question.answer_count,
+                question.user_supplied_answers,
+                answers));
+        }
+
+        return new SurveyResults(questions);
+    }
+}
---
+++ frontend/ts/entities/survey_results.ts
@@ -0,0 +1,43 @@
+import Entity, { Collection } from './entity';
+
+export default class SurveyResults extends Entity {
+    public readonly Questions: SurveyResultsQuestion[];
+
+    public constructor(questions: SurveyResultsQuestion[]) {
+        super();
+
+        this.Questions = questions;
+    }
+}
+
+export class SurveyResultsQuestion extends Entity {
+    public readonly Id: number;
+    public readonly Text: string;
+    public readonly AnswerCount: number;
+    public readonly UserSuppliedAnswers: string[];
+    public readonly ClosedAnswers: SurveyResultsClosedAnswer[];
+
+    public constructor(id: number, text: string, answer_count: number, user_supplied_answers: string[], closed_answers: SurveyResultsClosedAnswer[]) {
+        super();
+
+        this.Id = id;
+        this.Text = text;
+        this.AnswerCount = answer_count;
+        this.UserSuppliedAnswers = user_supplied_answers;
+        this.ClosedAnswers = closed_answers;
+    }
+}
+
+export class SurveyResultsClosedAnswer extends Entity {
+    public readonly Id: number;
+    public readonly Text: string;
+    public readonly AnswerCount: number;
+
+    public constructor(id: number, text: string, answer_count: number) {
+        super();
+
+        this.Id = id;
+        this.Text = text;
+        this.AnswerCount = answer_count;
+    }
+}
