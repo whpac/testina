65f35132445e5898cee191145a72688de18838b8
Marcin Szwarc <marcinszwarc@hotmail.com>
2020-10-07 17:35:51 +0200

    Podzielono na dwie karty

--- frontend/ts/pages/account.ts
+++ frontend/ts/pages/account.ts
@@ -1,6 +1,9 @@
+import AccountCard from '../components/account/account_card';
+import SiteSettingsCard from '../components/account/site_settings_card';
 import Page from '../components/basic/page';
 
 export default class AccountPage extends Page {
+    protected AccountCard: AccountCard;
 
     public constructor() {
         super();
@@ -8,9 +11,17 @@ export default class AccountPage extends Page {
         let heading = document.createElement('h1');
         heading.textContent = 'Twoje konto';
         this.AppendChild(heading);
+
+        this.AccountCard = new AccountCard();
+        this.AppendChild(this.AccountCard);
+
+        let settings_card = new SiteSettingsCard();
+        this.AppendChild(settings_card);
     }
 
     async LoadInto(container: HTMLElement, params?: any) {
+        this.AccountCard.Populate();
+
         container.appendChild(this.Element);
     }
 
