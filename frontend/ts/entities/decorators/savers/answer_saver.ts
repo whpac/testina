29ca972806bd1674192451c7f810c7c208b63d47
Marcin Szwarc <marcinszwarc@hotmail.com>
2020-12-22 19:56:41 +0100

    Modyfikacja zapisu

--- frontend/ts/entities/decorators/savers/answer_saver.ts
+++ frontend/ts/entities/decorators/savers/answer_saver.ts
@@ -1,3 +1,8 @@
+import BaseException from '../../../exceptions/base';
+import XHR from '../../../network/xhr';
+import SaveException from '../../exceptions/save_exception';
+import TransactionException from '../../exceptions/transaction_exception';
+import ApiEndpoints from '../../loaders/apiendpoints';
 import Answer from '../../schemas/answer';
 import AnswerDecorator from '../abstract/answer_decorator';
 import TransactionalSaver from './transactional_saver';
@@ -40,42 +45,77 @@ export default class AnswerSaver extends AnswerDecorator implements Transactiona
         if(auto_commit) this.CommitSaveTransaction();
     }
 
+    /**
+     * Rozpoczyna transakcję zapisu odpowiedzi.
+     * 
+     * Jeżeli transakcja jest już w toku, zwraca wyjątek TransactionException.
+     */
     BeginSaveTransaction(): void {
-        if(this.IsTransactionBegun) throw new Error('Transakcja jest w toku. Nie można rozpocząć kolejnej.');
+        if(this.IsTransactionBegun) throw new TransactionException('Transakcja jest w toku. Nie można rozpocząć kolejnej.');
 
         this.IsTransactionBegun = true;
         this.TransactionData.clear();
     }
 
+    /**
+     * Zatwierdza transakcję zapisu odpowiedzi.
+     * 
+     * Jeżeli nie rozpoczęto transakcji, zwraca wyjątek TransactionException.
+     * 
+     * W razie wystąpienia błędu podczas zapisu odpowiedzi, zwraca wyjątek SaveException.
+     */
     async CommitSaveTransaction(): Promise<void> {
-        if(!this.IsTransactionBegun) throw new Error('Nie rozpoczęto transakcji. Nie można jej zatwierdzić.');
+        if(!this.IsTransactionBegun) throw new TransactionException('Nie rozpoczęto transakcji. Nie można jej zatwierdzić.');
 
         let request_payload: any = {};
         for(let [key, value] of this.TransactionData) {
             request_payload[key] = value;
         }
 
-        // TODO: Wyślij dane na serwer
+        try {
+            let result = await XHR.PerformRequest(ApiEndpoints.GetEntityUrl(this), 'PUT', request_payload);
+            let is_successful = result.Status == 204;
 
-        let result = true;
-        if(result) this.PopulateThisWithTransactionData();
-        else this.RollbackSaveTransaction();
+            if(is_successful) {
+                this.PopulateThisWithTransactionData();
+            } else {
+                this.RollbackSaveTransaction();
+                throw new SaveException('Nie udało się zapisać odpowiedzi.');
+            }
+        } catch(e) {
+            this.RollbackSaveTransaction();
+
+            let inner_exception = e;
+            if(!(inner_exception instanceof BaseException)) inner_exception = undefined;
+
+            throw new SaveException('Wystąpił błąd podczas zapisywania odpowiedzi.', inner_exception);
+        }
     }
 
+    /**
+     * Wycofuje transakcję zapisu odpowiedzi
+     */
     RollbackSaveTransaction(): void {
         this.IsTransactionBegun = false;
         this.TransactionData.clear();
     }
 
-    PopulateThisWithTransactionData(): void {
-        let functions: { [key: string]: undefined | ((param: any) => void); } = {
+    /**
+     * Wypełnia odpowiedź danymi transakcji
+     */
+    protected PopulateThisWithTransactionData(): void {
+        let functions: { [key: string]: ((param: any) => void); } = {
             text: this.Answer.SetText,
             order: this.Answer.SetOrder,
             correct: this.Answer.SetIsCorrect
         };
 
+        this.SilenceEvent('changed');
         for(let [key, value] of this.TransactionData) {
             functions[key]?.(value);
         }
+
+        this.UnsilenceEvent('changed');
+        this.FireEvent('changed');
     }
 }
